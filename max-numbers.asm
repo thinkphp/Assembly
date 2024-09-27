3d04e7966b9a8ed89f64fc9528391d8dd4fb2732
Adrian Statescu <mergesortv@gmail.com>
2024-09-27 13:10:46 +0300

    Update and rename max.asm to max-numbers.asm

--- max-numbers.asm
+++ max-numbers.asm
@@ -1,3 +1,5 @@
+default rel
+
 section .data
     prompt1 db "Enter the first number: ", 0
     prompt2 db "Enter the second number: ", 0
@@ -18,22 +20,22 @@ main:
     mov rbp, rsp
 
     ; Prompt and read first number
-    mov rdi, prompt1
+    lea rdi, [prompt1]
     xor eax, eax
     call printf
 
-    mov rdi, fmt_input
-    mov rsi, num1
+    lea rdi, [fmt_input]
+    lea rsi, [num1]
     xor eax, eax
     call scanf
 
     ; Prompt and read second number
-    mov rdi, prompt2
+    lea rdi, [prompt2]
     xor eax, eax
     call printf
 
-    mov rdi, fmt_input
-    mov rsi, num2
+    lea rdi, [fmt_input]
+    lea rsi, [num2]
     xor eax, eax
     call scanf
 
@@ -45,7 +47,7 @@ main:
     mov rax, rbx      ; If num2 > num1, move num2 to rax
 
 print_result:
-    mov rdi, fmt_output
+    lea rdi, [fmt_output]
     mov rsi, rax
     xor eax, eax
     call printf
